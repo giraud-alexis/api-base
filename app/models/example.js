de5bb507c75c597f1b7616de6aa5d2b855e91285
giraud-alexis <alexis33190@gmail.com>
2025-02-01 22:28:39 +0100

    correctif model

--- app/models/example.js
+++ app/models/example.js
@@ -1,2 +1,21 @@
 import sequelize from "../database.js";
 import { Model, DataTypes } from "sequelize";
+
+export class nomDuModel extends Model {}
+nomDuModel.init(
+  {
+    example: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    example: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "nom de la table de la bdd",
+    timestamps: false,
+  }
+);
